acbacc151eeadc193c082686274a4c9dfe073698
Darjeeling47 <supawit.ma@gmail.com>
2024-05-30 17:57:48 +0700

    upload image

--- src/libs/MyProjects.tsx
+++ src/libs/MyProjects.tsx
@@ -10,7 +10,10 @@ export default function MyProjects() {
           codingLang: ["Java", "JavaFX"],
           description:
             "The PC application is a base game where players command their military, featuring two modes: Single Player and Two Player. At the start, a 25 x 25 map of terrains is randomly generated, including Land, Highland, Water, Bridge, and Town, each offering unique buffs and abilities. Players begin with various units: 1 Ship, 1 Destroyer Tank, 1 Heavy Tank, 2 Medium Tanks, 2 Light Tanks, 1 Sniper Soldier, 3 Heavy Soldiers, and 6 Normal Soldiers, each with distinct stats and skills such as activating a shield or grouping with another soldier. The objective is to defeat the opposing team, achieved either by eliminating all enemy units or invading the enemy's town. Each turn, players have 25 mana to perform actions, with the system providing assistance by identifying targets within range and finding the most efficient paths, making gameplay more intuitive.<br>I organized the project, set up the structure of the application, and worked on all of the algorithms, ensuring a smooth and engaging gameplay experience.",
-          image: "",
+          image: [
+            "/project/commanders-gambit-0.png",
+            "/project/commanders-gambit-1.png",
+          ],
           link: "",
         },
         {
@@ -26,7 +29,13 @@ export default function MyProjects() {
           ],
           description:
             "The web-based application allows users to reserve campgrounds efficiently. The system features a comprehensive database of campgrounds and individual campground sites, each with a unique area number and dimensions. Users can easily check the availability of a campground site for specific dates and make reservations, with a maximum of three pre-reserves per user. Additionally, the system empowers campground owners to create and promote their campgrounds.<br>As the Scrum Master and Project Manager, I oversaw the project, established the frontend and database structure, and contributed as a full-stack developer, ensuring a seamless and user-friendly experience for both campers and campground owners.",
-          image: "",
+          image: [
+            "/project/campground-reservation-system-0.png",
+            "/project/campground-reservation-system-1.png",
+            "/project/campground-reservation-system-2.png",
+            "/project/campground-reservation-system-3.png",
+            "/project/campground-reservation-system-4.png",
+          ],
           link: "",
         },
         {
@@ -42,7 +51,14 @@ export default function MyProjects() {
           ],
           description:
             "The system helps tutors efficiently and securely organize payments and manage students. Users can create organizations within the system, each accommodating multiple roles such as teachers, accountants, and administrators. Users can also create new classes to organize students, with each class containing payment details such as the payment amount and due date. The system allows parents to make payments using a Line bot, which automatically checks and validates receipts, displaying the data to the organization.<br>As the CEO and Project Manager, I established the overall structure and contributed as a full-stack developer, ensuring the system effectively reduces the time tutors spend on managing student payments and organization.",
-          image: "",
+          image: [
+            "/project/mindlink-tupay-0.png",
+            "/project/mindlink-tupay-1.png",
+            "/project/mindlink-tupay-2.png",
+            "/project/mindlink-tupay-3.png",
+            "/project/mindlink-tupay-4.png",
+            "/project/mindlink-tupay-5.png",
+          ],
           link: "",
         },
         {
@@ -58,25 +74,30 @@ export default function MyProjects() {
           ],
           description:
             "Friday Act System is a web-based application developed for CEDT at Chula Engineering to facilitate easier student activity applications. The system supports two roles: student and admin. Admins can create company profiles and activities associated with those companies. It also tracks each student's activity application history. The attendance function uses QR codes to validate student participation in the activities they applied for, ensuring accurate attendance records. This comprehensive data management provides a seamless and efficient experience for both admins and students.<br>As the Project Manager, I established the overall structure and contributed as a full-stack developer, ensuring the system is user-friendly and meets the needs of all users.",
-          image: "",
-          link: "",
-        },
-        {
-          topic: "LMT Stone Website",
-          startYear: "2024",
-          endYear: "Current",
-          codingLang: [
-            "Next.js",
-            "TailwindCSS",
-            "MongoDB",
-            "Express.js",
-            "Node.js",
+          image: [
+            "/project/friday-act-system-0.png",
+            "/project/friday-act-system-1.png",
+            "/project/friday-act-system-2.png",
+            "/project/friday-act-system-3.png",
           ],
-          description:
-            "LMT Stone is a customer-oriented website designed to showcase the various stone products offered by LMT Stone. The website serves as a platform to promote their products and services in a user-friendly manner. It features product categories, individual products, past projects, FAQs, contact information, and an 'about' section.<br>Additionally, the website is equipped with an admin interface, allowing administrators to easily add new products, projects, and FAQs using predefined forms. I was responsible for designing the user experience (UX/UI) and developing the database structure, collaborating closely with a full-stack development team to bring the vision to life.",
-          image: "",
           link: "",
         },
+        // {
+        //   topic: "LMT Stone Website",
+        //   startYear: "2024",
+        //   endYear: "Current",
+        //   codingLang: [
+        //     "Next.js",
+        //     "TailwindCSS",
+        //     "MongoDB",
+        //     "Express.js",
+        //     "Node.js",
+        //   ],
+        //   description:
+        //     "LMT Stone is a customer-oriented website designed to showcase the various stone products offered by LMT Stone. The website serves as a platform to promote their products and services in a user-friendly manner. It features product categories, individual products, past projects, FAQs, contact information, and an 'about' section.<br>Additionally, the website is equipped with an admin interface, allowing administrators to easily add new products, projects, and FAQs using predefined forms. I was responsible for designing the user experience (UX/UI) and developing the database structure, collaborating closely with a full-stack development team to bring the vision to life.",
+        //   image: ["/project/commanders-gambit.png"],
+        //   link: "",
+        // },
       ],
     },
     {
@@ -89,8 +110,12 @@ export default function MyProjects() {
           codingLang: ["React", "Bootstrap"],
           description:
             "The website is designed to showcase projects and provide information about JLControl.co. It encompasses all relevant company details, including an 'about us' section, contact information, and a portfolio of projects. It's a static webpage built with HTML, CSS, and JavaScript.<br>As a designer and front-end developer, my role involves crafting the visual and interactive elements of the site, ensuring a seamless user experience while effectively communicating JLControl.co's brand and services.",
-          image: "",
-          link: "https://jlcontrol.com",
+          image: [
+            "/project/jl-control-website-0.png",
+            "/project/jl-control-website-1.png",
+            "/project/jl-control-website-2.png",
+          ],
+          link: "https://www.jlcontrol.com/#/th",
         },
         {
           topic: "CEDT Library",
@@ -106,7 +131,7 @@ export default function MyProjects() {
           ],
           description:
             "The website is designed for CEDT students to communicate anonymously. It allows users to chat with each other in groups, functioning similarly to an open chat system. The user interface is designed to be friendly and suitable for students.<br>The system includes features like 'like' and 'report' options for messages to ensure a safe and positive community environment. As a front-end developer, my role involves creating an intuitive and engaging user experience to facilitate seamless interaction among students.",
-          image: "",
+          image: ["/project/cedt-library-0.png"],
           link: "",
         },
       ],
@@ -121,7 +146,12 @@ export default function MyProjects() {
           codingLang: ["HTML", "CSS", "Bootstrap", "PHP", "MySQL"],
           description:
             "This project is designed to enhance the capabilities of participants preparing for the POSN examination over a period of two months, covering all five major subjects. I am responsible for the computer science subject as well as overseeing the entire project. The training program includes 452 participants, and I serve as the founder and instructor for the computer science component, providing a total of 30 hours of instruction. In addition to teaching, I am also in charge of organizing and conducting guidance activities to ensure that participants receive comprehensive support throughout the training period.",
-          image: "",
+          image: [
+            "/project/mimir-project-0.png",
+            "/project/mimir-project-1.png",
+            "/project/mimir-project-2.png",
+            "/project/mimir-project-3.png",
+          ],
           link: "",
         },
       ],
@@ -143,7 +173,10 @@ export default function MyProjects() {
           ],
           description:
             "Project Classroom is a web-based application designed to enhance the connection and interaction between students and teachers. It offers a comprehensive set of tools that help teachers manage their students, assignments, and file sharing efficiently. Teachers can easily distribute assignments, upload resources, and track student progress through the platform. On the other hand, students benefit from a streamlined process for submitting assignments, downloading necessary files, and maintaining clear communication with their teachers. By centralizing these essential academic functions, Project Classroom fosters a more organized and collaborative educational environment, ultimately enhancing the learning experience for both students and teachers.",
-          image: "",
+          image: [
+            "/project/project-classroom-0.png",
+            "/project/project-classroom-1.png",
+          ],
           link: "",
         },
         {
@@ -153,7 +186,7 @@ export default function MyProjects() {
           codingLang: ["HTML", "CSS", "Bootstrap", "PHP", "MySQL"],
           description:
             "Package Tracker is a web-based application designed to help students at my school track their packages. When I was in boarding school, my friends frequently ordered items online and had them shipped to the school. This inspired the idea for the Package Tracker system. Users can track their packages by entering their name, and the system will display the package's status. Additionally, teachers who manage the packages can update and monitor the status to ensure that packages are properly handled and accounted for.",
-          image: "",
+          image: ["/project/package-tracker-0.png"],
           link: "",
         },
       ],
