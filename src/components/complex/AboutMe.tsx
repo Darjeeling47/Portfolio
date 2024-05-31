65befdc591fea3c46b3154221dda45102fe258b6
Supawit <supawit.ma@gmail.com>
2024-05-31 17:08:30 +0700

    clear text

--- src/components/complex/AboutMe.tsx
+++ src/components/complex/AboutMe.tsx
@@ -1,72 +1,45 @@
-import Image from "next/image";
-import Link from "next/link";
-import Contact from "./Contact";
-import Education from "./Education";
+import Image from "next/image"
+import Link from "next/link"
+import Contact from "./Contact"
+import Education from "./Education"
+import MyStory from "@/libs/MyStory"
 
 export default function AboutMe() {
+  const myStory = MyStory()
+
   return (
-    <div className="flex flex-col lg:flex-row gap-14 p-5 md:p-18 lg:p-20 xl:py-20 xl:px-40">
-      <div className="flex flex-col w-1/2 lg:w-1/3 gap-10">
-        <div className="w-full aspect-square relative rounded-3xl">
+    <div className='flex flex-col lg:flex-row gap-14 p-5 md:p-18 lg:p-20 xl:py-20 xl:px-40'>
+      <div className='flex flex-col w-1/2 lg:w-1/3 gap-10'>
+        <div className='w-full aspect-square relative rounded-3xl'>
           <Image
             src={"/profile-2.png"}
             alt={"profile"}
             fill
-            sizes="100vw"
-            objectFit="cover"
+            sizes='100vw'
+            objectFit='cover'
             priority={true}
-            className="rounded-3xl shadow-lg"
+            className='rounded-3xl shadow-lg'
           />
         </div>
-        <div className="hidden lg:block">
+        <div className='hidden lg:block'>
           <Contact />
         </div>
       </div>
-      <div className="flex flex-col w-full lg:w-2/3 gap-y-10">
-        <div className="flex flex-col gap-y-4">
-          <h1 className="font-semibold text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl text-my-gray-300">
+      <div className='flex flex-col w-full lg:w-2/3 gap-y-10'>
+        <div className='flex flex-col gap-y-4'>
+          <h1 className='font-semibold text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl text-my-gray-300'>
             Hi I'm Supawit Mahadthai
           </h1>
           <h2>A Software Developer, Project Manager</h2>
         </div>
-        <p>
-          ผมเป็นคนที่สนใจในการเขียนโปรแกรม และการจัดการบริหารต่าง ๆ
-          ซึ่งผมก็มีประสบการณ์ในการจัดการดูแลโครงการ
-          ซึ่งส่วนใหญ่จะเกี่ยวกับการพัฒนาบุคคลเป็นหลัก
-          ทั้งยังมีประสบการณ์ในการพัฒนาเว็บไซต์แบบ full stack
-          โดยบางโปรเจ็คก็ได้ชนะการแข่งขัน
-          ซึ่งตอนนี้อยู่ในช่วงการพัฒนาเพื่อนำมาใช้จริง
-          และมีโปรเจ็คบางตัวก็ได้มีการนำไปใช้จริงแล้วด้วย
-          ผมมีประสบการณ์การทำงานร่วมกับผู้อื่นทั้งในระดับโรงเรียน และระดับบริษัท
-          ซึ่งมีหลายตำแหน่งเช่น ผู้ดูแลโครงการ เป็นต้น
-        </p>
-        <p>
-          ผมเป็นคนที่สนใจในการเขียนโปรแกรม และการจัดการบริหารต่าง ๆ
-          ซึ่งผมก็มีประสบการณ์ในการจัดการดูแลโครงการ
-          ซึ่งส่วนใหญ่จะเกี่ยวกับการพัฒนาบุคคลเป็นหลัก
-          ทั้งยังมีประสบการณ์ในการพัฒนาเว็บไซต์แบบ full stack
-          โดยบางโปรเจ็คก็ได้ชนะการแข่งขัน
-          ซึ่งตอนนี้อยู่ในช่วงการพัฒนาเพื่อนำมาใช้จริง
-          และมีโปรเจ็คบางตัวก็ได้มีการนำไปใช้จริงแล้วด้วย
-          ผมมีประสบการณ์การทำงานร่วมกับผู้อื่นทั้งในระดับโรงเรียน และระดับบริษัท
-          ซึ่งมีหลายตำแหน่งเช่น ผู้ดูแลโครงการ เป็นต้น
-        </p>
-        <p>
-          ผมเป็นคนที่สนใจในการเขียนโปรแกรม และการจัดการบริหารต่าง ๆ
-          ซึ่งผมก็มีประสบการณ์ในการจัดการดูแลโครงการ
-          ซึ่งส่วนใหญ่จะเกี่ยวกับการพัฒนาบุคคลเป็นหลัก
-          ทั้งยังมีประสบการณ์ในการพัฒนาเว็บไซต์แบบ full stack
-          โดยบางโปรเจ็คก็ได้ชนะการแข่งขัน
-          ซึ่งตอนนี้อยู่ในช่วงการพัฒนาเพื่อนำมาใช้จริง
-          และมีโปรเจ็คบางตัวก็ได้มีการนำไปใช้จริงแล้วด้วย
-          ผมมีประสบการณ์การทำงานร่วมกับผู้อื่นทั้งในระดับโรงเรียน และระดับบริษัท
-          ซึ่งมีหลายตำแหน่งเช่น ผู้ดูแลโครงการ เป็นต้น
-        </p>
+        {myStory.map((story) => (
+          <p className='font-light'>{story}</p>
+        ))}
         <Education />
       </div>
-      <div className="lg:hidden">
+      <div className='lg:hidden'>
         <Contact />
       </div>
     </div>
-  );
+  )
 }
--- src/components/complex/MiniAboutMe.tsx
+++ src/components/complex/MiniAboutMe.tsx
@@ -1,33 +1,30 @@
-import Image from "next/image";
+import Image from "next/image"
 
 export default function MiniAboutMe() {
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-between gap-x-10 p-5 md:p-18 lg:p-20 xl:py-20 xl:px-40">
-      <div className="flex flex-col gap-y-3">
-        <h1 className="text-lg font-medium text-my-gray-300">About Me</h1>
-        <p>
-          ผมเป็นคนที่สนใจในการเขียนโปรแกรม และการจัดการบริหารต่าง ๆ
-          ซึ่งผมก็มีประสบการณ์ในการจัดการดูแลโครงการ
-          ซึ่งส่วนใหญ่จะเกี่ยวกับการพัฒนาบุคคลเป็นหลัก
-          ทั้งยังมีประสบการณ์ในการพัฒนาเว็บไซต์แบบ full stack
-          โดยบางโปรเจ็คก็ได้ชนะการแข่งขัน
-          ซึ่งตอนนี้อยู่ในช่วงการพัฒนาเพื่อนำมาใช้จริง
-          และมีโปรเจ็คบางตัวก็ได้มีการนำไปใช้จริงแล้วด้วย
-          ผมมีประสบการณ์การทำงานร่วมกับผู้อื่นทั้งในระดับโรงเรียน และระดับบริษัท
-          ซึ่งมีหลายตำแหน่งเช่น ผู้ดูแลโครงการ เป็นต้น
+    <div className='flex flex-col lg:flex-row lg:justify-between gap-x-10 p-5 md:p-18 lg:p-20 xl:py-20 xl:px-40'>
+      <div className='flex flex-col gap-y-3'>
+        <h1 className='text-lg font-medium text-my-gray-300'>About Me</h1>
+        <p className='font-light'>
+          I am passionate about programming and project management, with
+          experience in full-stack web development and personal development
+          projects. My work has won competitions and been implemented. I have
+          led teams at both school and corporate levels as a project manager. I
+          am excited about the future and committed to making a positive impact
+          on the world.
         </p>
       </div>
-      <div className="w-full aspect-video relative rounded-3xl hidden lg:block rotate-6 min-w-52 z-[50]">
+      <div className='w-full aspect-video relative rounded-3xl hidden lg:block rotate-6 min-w-52 z-[50]'>
         <Image
           src={"/profile-3.png"}
           alt={"profile"}
           fill
-          sizes="100vw"
-          objectFit="cover"
+          sizes='100vw'
+          objectFit='cover'
           priority={true}
-          className="rounded-3xl shadow-lg"
+          className='rounded-3xl shadow-lg'
         />
       </div>
     </div>
-  );
+  )
 }
