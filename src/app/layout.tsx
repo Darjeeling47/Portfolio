d934ffe875f4ece0a1f98843dd1b2ae740bbac3c
Darjeeling47 <supawit.ma@gmail.com>
2024-05-31 21:56:47 +0700

    test icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,38 @@
-import type { Metadata } from "next"
-import { ThemeProvider } from "next-themes"
-import { Prompt } from "next/font/google"
-import "./globals.css"
-import Navbar from "@/components/navbar/Navbar"
-import Bottom from "@/components/bottom/bottom"
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { Prompt } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Bottom from "@/components/bottom/bottom";
 
 const prompt = Prompt({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin", "thai"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Supawit Mahadthai",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
-        className={`${prompt.className} dark:bg-zinc-900 dark:text-zinc-400`}>
-        <ThemeProvider attribute='class'>
+        className={`${prompt.className} dark:bg-zinc-900 dark:text-zinc-400`}
+      >
+        <ThemeProvider attribute="class">
           <Navbar />
-          <main className='container mx-auto pt-12 mb-10'>{children}</main>
+          <main className="container mx-auto pt-12 mb-10">{children}</main>
           <Bottom />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
